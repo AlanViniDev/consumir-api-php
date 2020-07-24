3be6aed30b3e9089f8566ff1f2022e8470e1362a
Alan Vinicius <alan123vinicius@gmail.com>
2020-07-24 15:31:42 -0300

    Add files via upload

---
+++ consumir.php
@@ -0,0 +1,23 @@
+<?php
+
+/* url da api */
+$url_api = "http://localhost/API-REST/api/v1/estoque/mostrar";
+
+/* inicia a api */
+$iniciar = curl_init($url_api);
+
+/* converte a resposta em um array e não deixa em uma unica string*/
+curl_setopt($iniciar,CURLOPT_RETURNTRANSFER,true);
+
+/* verifica a ssl */
+curl_setopt($iniciar,CURLOPT_SSL_VERIFYPEER,false);
+
+/* Converte json para objeto php */
+$acessa = json_decode(curl_exec($iniciar));
+
+/* lista os dados */
+foreach($acessa->dados as $coluna) {
+    //var_dump($coluna);
+    echo $coluna->produto;
+}
+
